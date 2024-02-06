f1f31daa4679fe975d315383891e9bdc61b7667f
Ignas <ignaspanava@gmail.com>
2024-02-06 12:54:46 -0500

    Initial Commit

---
+++ TradingJournalApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TradingJournalApp
+//
+//  Created by Ignas Panavas on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TradingJournalApp/TradingJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingJournalAppApp.swift
+//  TradingJournalApp
+//
+//  Created by Ignas Panavas on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TradingJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
